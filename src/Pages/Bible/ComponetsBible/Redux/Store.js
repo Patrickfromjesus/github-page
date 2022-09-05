26f7d39afaa90682ab97f90a1ddf9cb9bd236af1
Gomes Conceição <patrick_epcar17@outlook.com>
2022-09-05 18:09:49 -0300

    remoção do devtools.

--- src/Pages/Bible/ComponetsBible/Redux/Store.js
+++ src/Pages/Bible/ComponetsBible/Redux/Store.js
@@ -1,8 +1,7 @@
-import { composeWithDevTools } from "@redux-devtools/extension";
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
